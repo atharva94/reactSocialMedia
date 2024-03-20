1d9d30fd4342997e81b3bb6682f99f250f41e51c
vipuld <vipuld@heaptrace.com>
2024-03-20 12:33:25 +0530

    Abort controller

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,28 +1,41 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import {PostList as PostListData} from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage"
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () =>{
     const { postList, addInitialPosts } = useContext(PostListData);
+    const [ fetching, setFetching ] = useState(false);
 
     useEffect(()=>{
-        fetch('https://dummyjson.com/posts')
+        setFetching(true);
+
+        const controller = new AbortController();
+        const signal = controller.signal;
+
+        fetch('https://dummyjson.com/posts', { signal })
         .then(res => res.json())
-        .then(data => { addInitialPosts(data.posts) });
-    },[])
-    
-    const handleGetAllPostsClicked = () => {
-        
-    }
+        .then(data => { 
+            addInitialPosts(data.posts);
+            setFetching(false);
+        });
+
+        return () =>{
+            console.log("Cleaning up useEffect");
+            controller.abort();
+        }
+    },[]);
 
     return (
         <>
+            { fetching && <LoadingSpinner />}
             {
+                !fetching &&
                 postList.length == 0 && 
-                <WelcomeMessage onGetPostClicked={handleGetAllPostsClicked} />
+                <WelcomeMessage/>
             }
-            {postList.map((post)=> <Post key={post.id} post={post} />)}
+            { !fetching && postList.map((post)=> <Post key={post.id} post={post} />)}
         </>
     );
 }
